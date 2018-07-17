adea9ba87be8c0753820ebc45a30c0c94a9a8809
Cameron Gray <cameron@camerongray.me>
2018-07-17 22:28:43 +0100

    Moved heating module out of src directory in preparation for moving into own repo

---
+++ modules/heating/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name="Home Automation Heating",
+    version="1.0",
+    description="Home Automation Hub Module - Heating",
+    author="Cameron Gray",
+    author_email="development@camerongray.me",
+    url="https://github.com/camerongray1515",
+    install_requires=[
+        "python-dateutil==2.7.3"
+    ],
+    packages=["home_automation.heating"],
+)
